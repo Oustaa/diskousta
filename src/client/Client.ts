14eb421e0e7459bccc5277394e3149447baf2403
Oustaa <otailaba98@gmail.com>
2024-05-15 23:13:13 +0100

    playing around with some ideas to see wich one works

---
+++ src/client/Client.ts
@@ -0,0 +1,46 @@
+const clearLine: (dir: 0 | 1 | -1) => Promise<boolean> = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve(true);
+    });
+  });
+};
+
+const moveCursor: (dx: number, dy: number) => Promise<boolean> = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve(true);
+    });
+  });
+};
+
+// a singleton emplementation of a terminal instance to handle the user input
+class Client {
+  private static _instance: Client | null = null;
+  public username: string;
+
+  constructor() {
+    this.username = "OUSTA6";
+  }
+
+  public static getClient(): Client {
+    if (!Client._instance) {
+      Client._instance = new Client();
+    }
+
+    return Client._instance;
+  }
+
+  public login(data: { email: string; password: string }): void {
+    const { email, password } = data;
+
+    console.log({ email, password });
+  }
+
+  public register(): void {}
+
+  public logout(): void {}
+}
+
+module.exports = Client;
+
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,39 @@
 const net = require("node:net");
+const readline = require("node:readline/promises");
+const { stdin: input, stdout: output } = require("node:process");
 
-const PORT = process.env.PORT || 8000;
-const HOST = process.env.HOST || "127.0.0.1";
+const Client = require("./Client");
 
-const clientSocket = net.connect({
-  port: PORT,
-  host: HOST,
-});
+const rl = readline.createInterface({ input, output });
+
+const client = Client.getClient();
+
+const loginCredentials = async (): Promise<{
+  email: string;
+  password: string;
+}> => {
+  return { email: "string", password: "string" };
+};
+
+(async () => {
+  let answer: number = -1;
+  console.log("#################### WELCOME TO DISKOSTA ####################");
+  console.log(`> 0 to quit\n> 1 CREATE ACCOUNT\n> 2 LOGIN TO YOUR ACCOUNT`);
+  while (answer !== 0) {
+    answer = Number(
+      await rl.question(
+        "> please Enter the number correspond to acction want to preform: "
+      )
+    );
+
+    switch (answer) {
+      case 1:
+        await client.login(await loginCredentials());
+        break;
+      case 2:
+        await client.register();
+        break;
+    }
+  }
+})();
 
